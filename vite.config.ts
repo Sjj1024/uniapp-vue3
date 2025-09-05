0033edee83d47bd9cd4e0b9389b70cc08afff44f
1024小神 <15670339118@163.com>
2025-09-05 21:05:02 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
-import path from "path";
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-  return {
-    server: {
-      host: "0.0.0.0",
-    },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-        "~": path.resolve(__dirname, "."),
-      },
-    },
-    plugins: [uni()],
-  };
-});
+    return {
+        server: {
+            host: '0.0.0.0',
+            allowedHosts: ['localhost', '192.168.1.100'],
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+                '~': path.resolve(__dirname, '.'),
+            },
+        },
+        plugins: [uni()],
+    }
+})
