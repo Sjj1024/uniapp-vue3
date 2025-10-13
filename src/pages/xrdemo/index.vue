c2a69bddc574030d417bd875288e92a5fa6f1154
1024小神 <15670339118@163.com>
2025-10-13 18:51:54 +0800

    Refactor XR demo page to use script setup syntax and streamline template structure. Removed unnecessary data properties and lifecycle methods for improved clarity and performance.

--- src/pages/xrdemo/index.vue
+++ src/pages/xrdemo/index.vue
@@ -1,45 +1,19 @@
 <template>
-    <view class="content">
-        <xr-ar-tracker
-            :width="renderWidth"
-            :height="renderHeight"
-            :style="
-                'width:' + screenWidth + 'px;height:' + screenHeight + 'px;'
-            "
-        >
-        </xr-ar-tracker>
-    </view>
+    <xr-ar-tracker
+        :width="renderWidth"
+        :height="renderHeight"
+        :style="'width:' + screenWidth + 'px;height:' + screenHeight + 'px;'"
+    >
+    </xr-ar-tracker>
 </template>
 
-<script>
+<script setup lang="ts">
 import {
     screenWidth,
     screenHeight,
     renderWidth,
     renderHeight,
-    windowWidth,
-    windowHeight,
 } from '@/utils/comm'
-
-export default {
-    data() {
-        return {
-            width: windowWidth,
-            height: windowHeight,
-            renderWidth: renderWidth,
-            renderHeight: renderHeight,
-            screenWidth: screenWidth,
-            screenHeight: screenHeight,
-        }
-    },
-    onLoad() {
-        console.log('windowWidth', windowWidth)
-        console.log('windowHeight', windowHeight)
-        console.log('renderWidth', renderWidth)
-        console.log('renderHeight', renderHeight)
-    },
-    methods: {},
-}
 </script>
 
 <style>
