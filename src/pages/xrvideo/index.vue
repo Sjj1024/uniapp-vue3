941994bc191874963a7142279db292622489d2c4
1024小神 <15670339118@163.com>
2025-10-16 18:19:55 +0800

    Adjust XR 2D video component styles for improved iOS fullscreen experience and update mesh scale for better video aspect ratio handling.

--- src/pages/xrvideo/index.vue
+++ src/pages/xrvideo/index.vue
@@ -2,12 +2,15 @@
     <xr-2d-video
         :width="renderWidth"
         :height="renderHeight"
-        :style="'width:' + screenWidth + 'px;height:' + screenHeight + 'px;'"
+        :style="
+            'width:' + (screenWidth + 150) + 'px;height:' + screenHeight + 'px;'
+        "
     >
     </xr-2d-video>
 </template>
 
 <script setup lang="ts">
+// screenWidth + 150 是为了让ios全屏的时候，内容没有拉伸
 import {
     screenWidth,
     screenHeight,
