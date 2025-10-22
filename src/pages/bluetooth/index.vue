541bfcb0c0dd000deb181bbac67d13bf7514745a
1024小神 <15670339118@163.com>
2025-10-22 18:05:42 +0800

    Update XR 2D video component styles for better iOS fullscreen compatibility and adjust mesh scale for optimal video aspect ratio.

---
+++ src/pages/bluetooth/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div>蓝牙列表</div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+// 搜索到的蓝牙
+const blueList = ref([])
+
+const discover = () => {
+    console.log('discover')
+    wx.startBluetoothDevicesDiscovery({
+        success: () => {
+            console.log('blue success')
+        },
+        fail: () => {
+            console.log('blue fail')
+        },
+    })
+}
+
+onMounted(() => {
+    discover()
+})
+</script>
+
+<style scoped lang="scss"></style>
